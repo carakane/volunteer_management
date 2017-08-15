8feb0afbfb53273c4dd367175ac6f89efaffb986
jessica fletcher <cara.kane@gmail.com>
2017-08-15 17:49:36 +0000

    add volunteer

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  resources :volunteers
+
   devise_for :users
   root 'welcome#index'
   # The priority is based upon order of creation: first created -> highest priority.
