3c74ccca3786872b334140c1891d6adcdad87bc5
jessica fletcher <cara.kane@gmail.com>
2017-08-16 13:46:48 +0000

    fix omniauth init

--- db/migrate/20170815171832_devise_create_users.rb
+++ db/migrate/20170815171832_devise_create_users.rb
@@ -2,6 +2,8 @@ class DeviseCreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
       t.string :uid
+      t.string :provider
+
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
@@ -34,6 +36,9 @@ class DeviseCreateUsers < ActiveRecord::Migration
 
       t.timestamps null: false
     end
+
+    add_index :users, :provider
+    add_index :users, :uid, unique: true
 
     add_index :users, :email,                unique: true
     add_index :users, :reset_password_token, unique: true
