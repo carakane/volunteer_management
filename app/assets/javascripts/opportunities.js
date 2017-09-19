dd571a0adffd8872958cc6277745603b970dd305
jessica fletcher <cara.kane@gmail.com>
2017-09-19 16:53:00 +0000

    attempt to render page via jQuery

---
+++ app/assets/javascripts/opportunities.js
@@ -0,0 +1,9 @@
+$(function () {
+  var opportunities = document.getElementById("opportunities");
+  var id = document.getElementsByTagName("a")[4].pathname.substring(7)
+  var getting = $.get('/users/' + id.json);
+  getting.done(function(data){
+    $("#body").append(data)
+    debugger;
+  })
+})
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,12 @@ class UsersController < ApplicationController
   def show
     @user = current_user
     @opportunities = Opportunity.by_organization(@user.organizations).most_recent.open
+    # respond_to do |f|
+    #   f.html { render :show }
+    #   f.json { render json: @opportunities }
+    # end
+
+     render json: @opportunities
   end
 
 end
