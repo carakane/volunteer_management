94dbb60b4a8a57ac69e53ec49cac4ffaa5dfcca5
jessica fletcher <cara.kane@gmail.com>
2017-09-24 17:11:33 +0000

    fix issue with turbolinks not reloading page

--- app/assets/javascripts/volunteers_show.js
+++ app/assets/javascripts/volunteers_show.js
@@ -1,5 +1,36 @@
-$(".volunteers.show").ready(function () {
+function loadVol(id) {
+  $("#next").attr({"volunteer": id})
+  $("#previous").attr({"volunteer": id})
+  document.getElementsByTagName("form")[0]["action"] = '/volunteers/'+ id +'/edit'
+  document.getElementsByTagName("form")[1]["action"] = '/volunteers/'+ id
+  // debugger;
 
+  var getting = $.get('/volunteers/' + id + '.json');
+  getting.done(function(data){
+    var vol = new Volunteer(data["id"], data["name"])
+
+    $.each(data.opportunities, function(k, v) {
+      // debugger;
+      var opp = new Opportunity(v["id"], v["name"], v["day"], v["status"])
+      var html = opp.url()  + ' || Day: ' + opp.day  + ' || Status: ' + opp.status + '<br />'
+      $("#opportunities").append(html)
+    })
+
+    var availability = new Availability(data.availability["id"], data.availability["volunteer_id"], data.availability["monday"], data.availability["tuesday"], data.availability["wednesday"], data.availability["thursday"], data.availability["friday"], data.availability["saturday"], data.availability["sunday"])
+    $("#monday").text(availability.monday)
+    $("#tuesday").text(availability.tuesday)
+    $("#wednesday").text(availability.wednesday)
+    $("#thursday").text(availability.thursday)
+    $("#friday").text(availability.friday)
+    $("#saturday").text(availability.saturday)
+    $("#sunday").text(availability.sunday)
+
+    $("#name").text(vol["name"])
+    })
+};
+
+
+var ready = function () {
     $("#next").click(function(){
       var volunteers = $.get('/volunteers.json');
       var nextVol;
@@ -45,37 +76,14 @@ $(".volunteers.show").ready(function () {
         }
       });
     });
-    function loadVol(id) {
-      $("#next").attr({"volunteer": id})
-      $("#previous").attr({"volunteer": id})
-      document.getElementsByTagName("form")[0]["action"] = '/volunteers/'+ id +'/edit'
-      document.getElementsByTagName("form")[1]["action"] = '/volunteers/'+ id
-      // debugger;
-
-      var getting = $.get('/volunteers/' + id + '.json');
-      getting.done(function(data){
-        var vol = new Volunteer(data["id"], data["name"])
-
-        $.each(data.opportunities, function(k, v) {
-          // debugger;
-          var opp = new Opportunity(v["id"], v["name"], v["day"], v["status"])
-          var html = opp.url()  + ' || Day: ' + opp.day  + ' || Status: ' + opp.status + '<br />'
-          $("#opportunities").append(html)
-        })
-
-        var availability = new Availability(data.availability["id"], data.availability["volunteer_id"], data.availability["monday"], data.availability["tuesday"], data.availability["wednesday"], data.availability["thursday"], data.availability["friday"], data.availability["saturday"], data.availability["sunday"])
-        $("#monday").text(availability.monday)
-        $("#tuesday").text(availability.tuesday)
-        $("#wednesday").text(availability.wednesday)
-        $("#thursday").text(availability.thursday)
-        $("#friday").text(availability.friday)
-        $("#saturday").text(availability.saturday)
-        $("#sunday").text(availability.sunday)
-
-        $("#name").text(vol["name"])
-        })
-    };
 
 
 loadVol(window.location.pathname.substring(12))
+}
+
+$(document).ready(ready);
+$(document).on('turbolinks:load', function() {;
+  if($(".volunteers.show").length > 0) {
+    ready();
+  }
 })
