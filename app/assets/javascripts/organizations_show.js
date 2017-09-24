6d2d807e7fb0938f00a4a8aa3a8bb1cdc4220c95
jessica fletcher <cara.kane@gmail.com>
2017-09-24 16:03:20 +0000

    fix organization show error

--- app/assets/javascripts/organizations_show.js
+++ app/assets/javascripts/organizations_show.js
@@ -1,14 +1,13 @@
-  $(".organizations.show").ready(function () {
-    var id = $("body")[0]["id"];
-    var getting = $.get('/organizations/' + id + '.json');
-    getting.done(function(data){
-        var org = new Organization(data["id"], data["name"]);
-
-        $.each(data["opportunities"], function(k, v) {
-          var opp = new Opportunity(v["id"], v["name"], v["day"], v["status"], org["id"]);
-          var html = opp.url()  + ' || Day: ' + opp.day  + ' || Status: ' + opp.status + '<br />';
-          $("#opportunities").append(html);
-        })
-        $("#organization").append(org["name"]);
-    });
-  })
+$(".organizations.show").ready(function () {
+  var id = window.location.pathname.substring(15);
+  var getting = $.get('/organizations/' + id + '.json');
+  getting.done(function(data){
+      var org = new Organization(data["id"], data["name"]);
+      $("#organization").append(org["name"]);
+      $.each(data["opportunities"], function(k, v) {
+        var opp = new Opportunity(v["id"], v["name"], v["day"], v["status"], org["id"]);
+        var html = opp.url()  + ' || Day: ' + opp.day  + ' || Status: ' + opp.status + '<br />';
+        $("#opportunities").append(html);
+      })
+  });
+})
