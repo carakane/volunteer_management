a9271f40aa7f7be438b32a95f654b673153a9e8f
jessica fletcher <cara.kane@gmail.com>
2017-09-24 12:43:31 +0000

    move js to asset pipeline

--- app/assets/javascripts/organizations.js
+++ app/assets/javascripts/organizations.js
@@ -1,11 +1,9 @@
-// $(function () {
-//   // return unless $('.organizations.index').length != 0
-//   var getting = $.get('/organizations.json');
-//   getting.done(function(data){
-//     $.each(data, function(k, v) {
-//       var html = '<a href="/organizations/' + data[k]["id"] + '">' + data[k]["name"] + '</a><br />';
-//       $("#organizations").append(html)
-//       // debugger;
-//     });
-//   });
-// })
+$(".organizations.index").ready(function () {
+  var getting = $.get('/organizations.json');
+  getting.done(function(data){
+    $.each(data, function(k, v) {
+      var org = new Organization(v["id"], v["name"])
+      $("#organizations").append(org.url())
+    });
+  });
+})
