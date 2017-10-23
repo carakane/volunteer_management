2a3b2e6da1cf4cea0b660d6d58a74adcde3d8ad5
c <github email address>
2017-10-23 13:50:24 -0400

    migrate to postgresql

--- db/migrate/20170817140538_availability.rb
+++ db/migrate/20170817140538_availability.rb
@@ -2,13 +2,13 @@ class Availability < ActiveRecord::Migration
   def change
     create_table :availabilities do |t|
       t.integer :volunteer_id
-      t.string :monday, null: false, default: false
-      t.string :tuesday, null: false, default: false
-      t.string :wednesday, null: false, default: false
-      t.string :thursday, null: false, default: false
-      t.string :friday, null: false, default: false
-      t.string :saturday, null: false, default: false
-      t.string :sunday, null: false, default: false
+      t.boolean :monday, null: false, default: false
+      t.boolean :tuesday, null: false, default: false
+      t.boolean :wednesday, null: false, default: false
+      t.boolean :thursday, null: false, default: false
+      t.boolean :friday, null: false, default: false
+      t.boolean :saturday, null: false, default: false
+      t.boolean :sunday, null: false, default: false
     end
   end
 end
--- db/migrate/20170817163202_change_opportunities.rb
+++
@@ -1,5 +0,0 @@
-class ChangeOpportunities < ActiveRecord::Migration
-  def change
-    add_column :opportunities, :day, :string
-  end
-end
