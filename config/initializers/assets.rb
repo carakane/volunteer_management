eaf151ce93faa440ade8fc0578f5e0395dc83a78
c <github email address>
2017-10-23 16:41:56 -0400

    add precompile instructions to fix asset error

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,3 +9,9 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+
+Rails.application.config.assets.precompile += %w( volunteers_show.js )
+Rails.application.config.assets.precompile += %w( volunteers_new.js )
+Rails.application.config.assets.precompile += %w( prototypes.js )
+Rails.application.config.assets.precompile += %w( organizations_index.js )
+Rails.application.config.assets.precompile += %w( organizations_new.js )
